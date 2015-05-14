68451cb9f0e2511a3522cc93a78b27a45b83d44d
Chris McDonald <cjmcdona@butler.edu>
2015-05-14 11:35:16 -0400

    debugging cfd to find where card info is stored

--- utils/analyzers/progress_charts_analyzer.rb
+++ utils/analyzers/progress_charts_analyzer.rb
@@ -113,7 +113,7 @@ class ProgressChartsAnalyzer
 
       inScopeLists.each do |list|
         inCount += cfd[date][list["name"]]
-        print cfd
+        print "THIS IS THE TEST OF CFD: " + cfd
         #logger.info {"cfd[#{date}][#{list}['name'] = #{cfd[date][list['name']]}"}
       end
       inList_array << [date.strftime('%s000').to_i, inCount]
