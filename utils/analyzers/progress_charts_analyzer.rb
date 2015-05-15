2e41a96e56b9e00db4e3cc8516ad090e144d4ad9
bomattin <bomattin@butler.edu>
2015-05-15 08:54:13 -0400

    Mas logic corrections

--- utils/analyzers/progress_charts_analyzer.rb
+++ utils/analyzers/progress_charts_analyzer.rb
@@ -100,9 +100,9 @@ class ProgressChartsAnalyzer
 
 
         if action["type"] == "updateCard" && !data["listAfter"].nil? && !data["listBefore"].nil?
-          list = data["listBefore"]
+          list = data["listAfter"]
 
-          matching_list = open_lists.select {|l| l["id"] == data["listAfter"]["id"]}.first
+          matching_list = open_lists.select {|l| l["id"] == data["listBefore"]["id"]}.first
           unless matching_list.nil?
             cfd[date][matching_list["name"]].delete data["card"]["id"]
             cfdpoints[date][matching_list["name"]].delete data["card"]["points"]
