bce73dd59f323ed015da0ae55ddb676d9567127e
bomattin <bomattin@butler.edu>
2015-05-15 09:09:00 -0400

    i'm nt very smrt

--- utils/analyzers/progress_charts_analyzer.rb
+++ utils/analyzers/progress_charts_analyzer.rb
@@ -137,7 +137,8 @@ class ProgressChartsAnalyzer
     ap cfdpoints
 
     cfd.each {|k,v| v.each {|l,c| cfd[k][l] = c.count}}
-    cfdpoints.each {|k,v| v.each {|l,c| cfd[k][l] = l.inject(:+)}}
+    #cfdpoints.each {|k,v| v.each {|l,c| cfd[k][l] = c.inject(:+)}}
+    cfdpoints.each {|k,v| v.each {|l,c| puts c}}
 
     # cfd.each {|k,v| v.each {|l,c| cfd[k][l] = c.count}}
     # c[i]["pointvalue"]
